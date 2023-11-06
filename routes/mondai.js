9b8629188ed9eb65bc7efe2cb615d9863f9bf44d
arton_bee <absowrd39.arton@gmail.com>
2023-11-06 14:21:35 +0900

    2023-11-06 14:20

--- routes/mondai.js
+++ routes/mondai.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   var name1 = req.query.name;
   var app = req.app;
-  const sql = "select name from mondai_LIST;"
+  const sql = "select question_name from question_table;"
   const poolCluster = app.get('pool');
   var pool = poolCluster.of('MASTER');
   pool.getConnection(function(err,connection){
@@ -19,7 +19,7 @@ router.get('/', function(req, res, next) {
         console.log(err);
       }
       var data = {
-        name:name1,
+        question_name:name1,
         web:result1
       }
       res.render('mondai2',data);
