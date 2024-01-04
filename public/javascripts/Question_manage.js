578792fc1e284810c4f30c500b97a171c59b7b25
myoujinn <nkhsha031010@g,neec,ac,jp>
2024-01-05 02:05:18 +0900

    プルされ手ほしい

--- public/javascripts/Question_manage.js
+++ public/javascripts/Question_manage.js
@@ -3,4 +3,6 @@ window.onload = function(){
   // ページ読み込み時に実行したい処理
   socket.emit('Question_manage');
   connection.query(Question(question_id,question_name,question_text,picture_flag,type_id))
+  console.log("なんかでた？");
+
   }  
