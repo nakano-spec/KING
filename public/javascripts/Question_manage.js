de347bf8a82d1efbb91810cf35dc541c0b553758
myoujinn <nkhsha031010@g,neec,ac,jp>
2024-01-10 20:59:44 +0900

    ioとかよくわからない

--- public/javascripts/Question_manage.js
+++ public/javascripts/Question_manage.js
@@ -1,8 +1,16 @@
-var socket = io();
-window.onload = function(){
-  // ページ読み込み時に実行したい処理
-  socket.emit('Question_manage');
-  connection.query(Question(question_id,question_name,question_text,picture_flag,type_id))
-  console.log("なんかでた？");
+function loadScript(url, callback) {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = url;
 
-  }  
+  script.onload = function() {
+      callback();
+  };
+
+  document.head.appendChild(script);
+}
+
+loadScript('bin/www', function() {
+  document.getElementById('content').innerText = getValueFromScript2();
+});
+
