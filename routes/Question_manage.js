5b692ffdac275b46879204f0001b084ff44fc004
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-15 14:15:39 +0900

    エラーがわからない

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('Question_manage.ejs');
+    res.render('javascripts/Question_manage.ejs');
   });
 module.exports = router;
