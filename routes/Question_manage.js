0eaeb407dcc6196035091dbb7b3d0682b406480c
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-15 15:25:25 +0900

    routesがよくわからない

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res){
-    res.render('javascripts/Question_manage.ejs');
-  });
+//router.get('/', function(_req, res){
+  //  res.render('javascripts/Question_manage.ejs');
+  //});
 module.exports = router;
+
+
+
+
