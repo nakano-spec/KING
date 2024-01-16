c1a663a0daaf8164b8dbcc8f5c7d243c14b21b77
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-16 16:18:08 +0900

    mondai3を参考にしてみた

--- public/javascripts/Question_manage.js
+++ public/javascripts/Question_manage.js
@@ -1,4 +1,4 @@
-function loadScript(url, callback) {
+/*function loadScript(url, callback) {
   var script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = url;
@@ -8,9 +8,18 @@ function loadScript(url, callback) {
   };
 
   document.head.appendChild(script);
-}
+}*/
 
 loadScript('bin/www', function() {
   document.getElementById('content').innerText = getValueFromScript2();
 });
+            f.button3.addEventListener('click',function(e){
+                e.preventDefault();
+                socket.emit('owa2');
+            })
+
+            f.kekka.addEventListener('click',function(e){
+                e.preventDefault();
+                window.location.href = '/Question_manage';
+            })
 
--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -1,11 +1,26 @@
-var express = require('express');
+const express = require("express");
 var router = express.Router();
+const mysql = require("mysql");
+/*const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "20021225",
+    database: "mydb"
+});*/
 
-//router.get('/', function(_req, res){
-  //  res.render('javascripts/Question_manage.ejs');
-  //});
+//connection.connect();
+
+router.get("/", (req, res)=>{
+    const sql = 'select * from Question_manage;'
+    var app = req.app;
+    var poolCluster = app.get("pool");
+    var pool = poolCluster.of('MASTER');
+            connection.release();
+    })
+
+
+
 module.exports = router;
-
 
 
 
