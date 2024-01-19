32799cb1f154b7cff9b88b6ea3c4ccd1af14a2e0
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-19 15:33:22 +0900

    逆も試してみる

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -6,7 +6,6 @@ var socket = io();
 /*loadでページが読み込まれたときに */
 socket.addEventListener('load',sampleEvent, false);
 function sampleEvent() {
-  let sql = 'select * from Question_manage;'
   connection.query(sql(question_id,question_name,question_text,picture_flag,type_id))
   if(question_id){
     console.log(question_id);
