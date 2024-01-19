a0f209f9e528e390ee054b2a3376d005a634df2f
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-19 18:03:36 +0900

    とりあえず、表示できるところまで戻したい

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -1,24 +1,5 @@
-/*const express = require("express");
-var router = express.Router();
-
-var socket = io();
-
-/*loadでページが読み込まれたときに 
-Question.addEventListener('load',sampleEvent, false);
-function sampleEvent() {
-  connection.query(sql(question_id,question_name,question_text,picture_flag,type_id))
-  if(question_id){
-    console.log(question_id);
-  }
-  //ここに処理を記述する
-  
-  module.exports = router;
-}*/
-app.get('/', function (req, res) {
-
-  res.send('ページが表示されました！');
-
-});
+const string = "革命はテレビでは放送されない。";
+console.log(string);
 
 
 
