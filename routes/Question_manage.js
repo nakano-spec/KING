35218cae24ad979c851247bfc5240dcee0421f13
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-23 13:38:13 +0900

    router.getを追加してみた

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -4,6 +4,10 @@ var router = express.Router();
 let str = "革命はテレビでは放送されない";
 console.log(str);
 
+router.get('/', function(req, res, next) {
+    res.render('Question_manage.js');
+  });
+
 module.exports = router;
 
 
