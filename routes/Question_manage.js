e7cf0561c97deec69b30eb221dda90a39983b596
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-23 15:09:26 +0900

    試し

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
+//const mysql = require("mysql");
 
-
-router.get('/', function(req, res, next) {
-    let str = "革命はテレビでは放送されない";
-　　console.log(str);
-    res.render('Question_manage');
-  });
+let str = "革命はテレビでは放送されない";
+console.log(str);
+res.render('kakunin.ejs',{data:str});
 
 module.exports = router;
 
