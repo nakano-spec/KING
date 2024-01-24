1a4a57a2820c2a3509cef9498281f8859852e7f2
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-24 14:08:25 +0900

    gtpにきいた

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -3,15 +3,32 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     var app = req.app;
-    var Questionsql = 'select * from Question_manage;'
+    var Questionsql = 'SELECT * FROM Question_manage;';
     var poolCluster = app.get("pool");
     var pool = poolCluster.of('MASTER');
-    pool.getConnection(function(err,connection){
-        connection.query(Questionsql,(err,results,fields)=>{
-        res.render('Question_manage', {Questionsql:results });
-        //res.render('hyouji4',{han1:results});
-        })
-        connection.release();
-    
+
+    pool.getConnection(function(err, connection) {
+        if (err) {
+            // コネクション取得時のエラー処理
+            console.error("DB connection error:", err);
+            res.status(500).send("Database connection error");
+            return;
+        }
+
+        connection.query(Questionsql, (err, results, fields) => {
+            connection.release(); // コネクションをリリース
+
+            if (err) {
+                // クエリ実行時のエラー処理
+                console.error("Query error:", err);
+                res.status(500).send("Database query error");
+                return;
+            }
+
+            // クエリの結果をビューに渡す
+            res.render('Question_manage', { results: results });
+        });
+    });
 });
+
 module.exports = router;
