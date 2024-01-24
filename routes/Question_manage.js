2769976b16c76ecf8658990bfdb5b60c72ebfd0a
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-24 16:38:53 +0900

    前回のはエラーはないが取ったデータが一つの変数に入っていたかも

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -3,13 +3,12 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     var app = req.app;
-    var Questionsql = 'SELECT * FROM question_table;';
+    var Questionsql = 'SELECT * FROM Question_manage;';
     var poolCluster = app.get("pool");
     var pool = poolCluster.of('MASTER');
 
     pool.getConnection(function(err, connection) {
         if (err) {
-            // コネクション取得時のエラー処理
             console.error("DB connection error:", err);
             res.status(500).send("Database connection error");
             return;
@@ -19,14 +18,13 @@ router.get('/', (req, res) => {
             connection.release(); // コネクションをリリース
 
             if (err) {
-                // クエリ実行時のエラー処理
                 console.error("Query error:", err);
                 res.status(500).send("Database query error");
                 return;
             }
 
             // クエリの結果をビューに渡す
-            res.render('Question_manage', { results: results });
+            res.render('Question_manage', { questions: results });
         });
     });
 });
