a268d89f6ab6920833bc1eb94f9b58381f7e439d
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-24 11:49:19 +0900

    SELECT文を入れてみた。

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -2,7 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('Question_manage', { text: 'Hello World!' });
+    var app = req.app;
+    var Questionsql = 'select * from Question_manage;'
+    var poolCluster = app.get("pool");
+    var pool = poolCluster.of('MASTER');
+    pool.getConnection(function(err,connection){
+        connection.query(Questionsql,(err,results,fields)=>{
+        res.render('Question_manage', {Questionsql:results });
+        //res.render('hyouji4',{han1:results});
+        })
+        connection.release();
+    
 });
-
 module.exports = router;
