a0c924e3d3c5cb67c171dc10b88e0df7ad891209
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-24 09:37:38 +0900

    何故かQuestionがコメントアウトされていた

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ var kanryouRouter = require('./routes/kanryou');
 var missRouter = require('./routes/miss');
 var uploadRouter = require('./routes/upload.js');
 var tuika2Router = require('./routes/tuika2');
-//var QuestionmanageRouter = require('./routes/Question_manage');
+var QuestionmanageRouter = require('./routes/Question_manage');
 
 const router = require('./routes/index');
 //読み込んだexpressをapp変数に格納
@@ -84,7 +84,7 @@ app.use('/kanryou',kanryouRouter);
 app.use('/miss',missRouter);
 app.use('/upload',uploadRouter);
 app.use('/tuika2',tuika2Router);
-//app.use('/Question_manage',QuestionmanageRouter);
+app.use('/Question_manage',QuestionmanageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
