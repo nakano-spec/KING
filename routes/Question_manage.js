82675e8b25721b2dfd894d33decd34edc29d9c8b
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-26 15:27:24 +0900

    GTPになおしてもらった

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -3,8 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     var app = req.app;
-    var Questionsql = 'SELECT question_name FROM question_table;';
-    var Questionsql2 = 'SELECT question_text FROM question_table;';
+    var QuestionSql = 'SELECT question_name, question_text FROM question_table;';
     var poolCluster = app.get("pool");
     var pool = poolCluster.of('MASTER');
 
@@ -15,28 +14,17 @@ router.get('/', (req, res) => {
             return;
         }
 
-        // 最初のクエリを実行
-        connection.query(Questionsql, (err, results1) => {
+        connection.query(QuestionSql, (err, results) => {
+            connection.release(); // コネクションをリリース
+
             if (err) {
-                connection.release();
                 console.error("Query error:", err);
                 res.status(500).send("Database query error");
                 return;
             }
 
-            // ２つ目のクエリを実行
-            connection.query(Questionsql2, (err, results2) => {
-                connection.release(); // コネクションをリリース
-
-                if (err) {
-                    console.error("Query error:", err);
-                    res.status(500).send("Database query error");
-                    return;
-                }
-
-                // 両方のクエリ結果をビューに渡す
-                res.render('Question_manage', { questionNames: results1, questionTexts: results2 });
-            });
+            // クエリの結果をビューに渡す
+            res.render('Question_manage', { questions: results });
         });
     });
 });
