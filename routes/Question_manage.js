08902c56e487bc4b10b141759d9c1d3dcede28b0
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-29 00:43:53 +0900

    コネクションリリースの場所を変更

--- routes/Question_manage.js
+++ routes/Question_manage.js
@@ -4,9 +4,11 @@ const router = express.Router();
 router.get('/', (req, res) => {
     var app = req.app;
     var QuestionSql = 'SELECT question_name, question_text FROM question_table;';
+    var QuestionDelete = 'DELETE FROM question_table WHERE question_name = QuestionName;';
     var poolCluster = app.get('pool2');
     var pool = poolCluster.of('MASTER');
 
+    //dbと接続できたか
     pool.getConnection(function(err1, connection) {
         if (err1) {
             console.error("DB connection error:", err1);
@@ -15,14 +17,14 @@ router.get('/', (req, res) => {
         }
 
         connection.query(QuestionSql, (err2, results) => {
-            connection.release(); // コネクションをリリース
+            
 
             if (err2) {
                 console.error("Query error:", err2);
                 res.status(500).send("Database query error");
                 return;
             }
-
+            connection.release(); // コネクションをリリース
             // クエリの結果をビューに渡す
             res.render('Question_manage', { questions: results });
         });
