608211eb32151c36450a1ff07256a287caf89f20
myoujinn-syuuya <nkhsha031010@g.neec.ac.jp>
2024-01-28 23:41:52 +0900

    KING-1でもとにもどしたい

--- routes/mondai2.js
+++ routes/mondai2.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
-//const mysql = require("mysql");
-const mysql = require('mysql');
-
+const mysql = require("mysql");
 /*const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
